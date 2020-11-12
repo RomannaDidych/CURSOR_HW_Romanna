2e4fb48cf18fc167a6f95fc45593dd66cadeaa8f
unknown <romanna.d@gmail.com>
2020-11-12 13:35:16 +0200

    really ready to test

--- HW_01/script_01.js
+++ HW_01/script_01.js
@@ -1,34 +1,43 @@
 //Base
-const priceThing1 = 15.678;
-const priceThing2 = 123.965; 
-const priceThing3 = 90.2345;
-console.log(Math.max(priceThing1, priceThing2, priceThing3));
-console.log(Math.min(priceThing1, priceThing2, priceThing3));
-const totalPrice = priceThing1 + priceThing2 + priceThing3;
+const pricePen = 15.678;
+const priceBook = 123.965; 
+const priceToy = 90.2345;
+const SumForPay = 500;
+const minDiscount = 5;
+const maxDiscount = 95;
+let maxPrice = Math.max(pricePen, priceBook, priceToy)
+console.log(maxPrice);
+let minPrice = Math.min(pricePen, priceBook, priceToy);
+console.log(minPrice);
+let totalPrice = pricePen + priceBook + priceToy;
 console.log(totalPrice);
-let integerSum = Math.trunc(priceThing1) + Math.trunc(priceThing2) + Math.trunc(priceThing3);
-let roundedSum = 100* Math.round(integerSum/100);
+let integerSum = Math.trunc(pricePen) + Math.trunc(priceBook) + Math.trunc(priceToy);
+let roundedSum = 100* Math.ceil(integerSum/100);
 console.log(roundedSum);
-console.log(` чи є сума всіх товарів (округлена в меншу сторону) парним числом?  ${integerSum/2 == Math.trunc(integerSum/2)}`);
-console.log(`решта від 500грн становить: ${500 - totalPrice}`);
-console.log(`середня ціна: ${Math.round((totalPrice/3)*100)/100}`);
-let discount = Math.floor(Math.random() * 100);
-console.log(`знижка: ${discount}%`);
-let discountPrice = Math.round((totalPrice - totalPrice*discount/100)*100)/100;
-console.log(`при знижці ${discount}% сума до оплати: ${discountPrice}грн`);
+let isEven  = integerSum%2 == 0;
+console.log(isEven);
+let restAfterPay = +((SumForPay - totalPrice).toFixed(2));
+console.log(restAfterPay);
+let averagePrice = +((totalPrice/3).toFixed(2));
+console.log(averagePrice);
+let discount = Math.floor(Math.random() * (maxDiscount - minDiscount +1)) + minDiscount;
+console.log(`${discount}%`);
+let discountPrice = +((totalPrice - totalPrice*discount/100).toFixed(2));
+console.log(discountPrice);
 let cost = Math.trunc(totalPrice)/2;
 let profit = cost - Math.trunc(totalPrice*discount/100);
 console.log(profit);
 
-//Advanced
-console.log(`максимальна ціна: ${Math.max(priceThing1, priceThing2, priceThing3)}грн
-	мінімальна ціна: ${Math.min(priceThing1, priceThing2, priceThing3)}грн 
-	вартість всіх товарів: ${totalPrice}грн
-	сума округлених (в меншу сторону) цін: ${integerSum} 
-	сума товарів округлена до сотень: ${roundedSum}
-	сума всіх товарів (округлена в меншу сторону) є парним числом: ${integerSum/2 == Math.trunc(integerSum/2)}
-	решта від 500грн становить: ${500 - totalPrice}грн
-	середня ціна: ${Math.round((totalPrice/3)*100)/100}грн
-	знижка: ${discount}%
-	при знижці ${discount}% сума до оплати: ${discountPrice}грн 
-	чистий прибуток: ${profit}грн`);
+//Advanced 
+console.log(`
+максимальна ціна: ${maxPrice}
+мінімальна ціна: ${minPrice} 
+вартість всіх товарів: ${totalPrice}
+сума округлених (в меншу сторону) цін: ${integerSum} 
+сума товарів округлена до сотень: ${roundedSum}
+сума всіх товарів (округлена в меншу сторону) є парним числом: ${isEven}
+решта від 500грн становить: ${restAfterPay}
+середня ціна: ${averagePrice}
+знижка: ${discount}%
+при знижці ${discount}% сума до оплати: ${discountPrice}
+чистий прибуток: ${profit}`);
