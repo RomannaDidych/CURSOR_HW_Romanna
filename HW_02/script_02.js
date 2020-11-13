3f92afdaddfb25c87279ad69b7ec40b5860beb9e
unknown <romanna.d@gmail.com>
2020-11-13 15:43:28 +0200

    entered two numbers

--- HW_02/script_02.js
+++ HW_02/script_02.js
@@ -1,6 +1,15 @@
-let minNumber = +(prompt(`Please, enter an integer number`));
-if (minNumber === NaN ){
-	for (let i = 0; i < 3; i++)
-}
+let minNumber = parseInt(prompt(`Please, enter a first integer number`));
+while (isNan(minNumber)){
+	minNumber = parseInt(prompt(`Uppps!!! You did not enter a number.\n Please enter a first integer number`));
+};
+let maxNumber = parseInt(prompt(`Please, enter a second integer number`));
+while (isNan(maxNumber)){
+	maxNumber = parseInt(prompt(`Uppps!!! You did not enter a number.\n Please enter a second integer number`));
+};
+if (minNumber > maxNumber){
+	const temp = minNumber;
+	minNumber = maxNumber;
+	maxNumber = temp;
+};
 
-for (let i = 0; i < 3; i++)
+
