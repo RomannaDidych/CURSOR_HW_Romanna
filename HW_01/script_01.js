2a72391598ba403020eec017f7ff8b1db75b4cb8
unknown <romanna.d@gmail.com>
2020-11-13 15:12:09 +0200

    fixed errors after verification

--- HW_01/script_01.js
+++ HW_01/script_01.js
@@ -5,27 +5,27 @@ const priceToy = 90.2345;
 const SumForPay = 500;
 const minDiscount = 5;
 const maxDiscount = 95;
-let maxPrice = Math.max(pricePen, priceBook, priceToy)
+const maxPrice = Math.max(pricePen, priceBook, priceToy)
 console.log(maxPrice);
-let minPrice = Math.min(pricePen, priceBook, priceToy);
+const minPrice = Math.min(pricePen, priceBook, priceToy);
 console.log(minPrice);
-let totalPrice = pricePen + priceBook + priceToy;
+const totalPrice = pricePen + priceBook + priceToy;
 console.log(totalPrice);
-let integerSum = Math.trunc(pricePen) + Math.trunc(priceBook) + Math.trunc(priceToy);
-let roundedSum = 100* Math.ceil(integerSum/100);
+const integerSum = Math.floor(pricePen) + Math.floor(priceBook) + Math.floor(priceToy);
+const roundedSum = 100* Math.ceil(integerSum/100);
 console.log(roundedSum);
-let isEven  = integerSum%2 == 0;
+const isEven  = integerSum%2 === 0;
 console.log(isEven);
-let restAfterPay = +((SumForPay - totalPrice).toFixed(2));
+const restAfterPay = +((SumForPay - totalPrice).toFixed(2));
 console.log(restAfterPay);
-let averagePrice = +((totalPrice/3).toFixed(2));
+const averagePrice = +((totalPrice/3).toFixed(2));
 console.log(averagePrice);
-let discount = Math.floor(Math.random() * (maxDiscount - minDiscount +1)) + minDiscount;
+const discount = Math.floor(Math.random() * (maxDiscount - minDiscount +1)) + minDiscount;
 console.log(`${discount}%`);
-let discountPrice = +((totalPrice - totalPrice*discount/100).toFixed(2));
+const discountPrice = +((totalPrice - totalPrice*discount/100).toFixed(2));
 console.log(discountPrice);
-let cost = Math.trunc(totalPrice)/2;
-let profit = cost - Math.trunc(totalPrice*discount/100);
+const cost = Math.trunc(totalPrice)/2;
+const profit = cost - Math.trunc(totalPrice*discount/100);
 console.log(profit);
 
 //Advanced 
---
+++ HW_02/script_02.js
@@ -0,0 +1,6 @@
+let minNumber = +(prompt(`Please, enter an integer number`));
+if (minNumber === NaN ){
+	for (let i = 0; i < 3; i++)
+}
+
+for (let i = 0; i < 3; i++)
