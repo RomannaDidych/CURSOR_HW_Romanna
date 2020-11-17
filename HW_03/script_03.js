52e20ca19235a24af4b3e9ebe0144045c355fd65
unknown <romanna.d@gmail.com>
2020-11-17 17:01:20 +0200

    getMaxDigit has written as an arrow function

--- HW_03/script_03.js
+++ HW_03/script_03.js
@@ -1,10 +1,10 @@
-function getMaxDigit(number){
+const getMaxDigit = number => {
 	let maxDigit = 0;
 	while (number >= 1){
 		  if (maxDigit < number%10) maxDigit = number%10;		  
 		  number = Math.trunc(number/10);
 	};
 	return maxDigit;
-};
-
+}
+ 
 console.log(`function №1 getMaxDigit(865513): ${getMaxDigit(865513)}`);
