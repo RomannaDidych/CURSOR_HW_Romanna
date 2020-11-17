b2b8a2c106239a6de5be0a81ad6e62d6b95db750
unknown <romanna.d@gmail.com>
2020-11-17 17:39:23 +0200

    added function #2 calculatePower

--- HW_03/script_03.js
+++ HW_03/script_03.js
@@ -6,5 +6,22 @@ const getMaxDigit = number => {
 	};
 	return maxDigit;
 }
+
+const calculatePower = (num, pow) => {
+	let result = 1;	
+	if (pow === 0) return result;
+	if (pow > 0){
+		for (let i = 1; i<=pow; i++){
+			result *= num; 
+		}
+	} else {		
+		for (let i = 1; i<=Math.abs(pow); i++){
+			result *= 1 / num;			 
+		}
+	};
+	return result;
+};
  
-console.log(`function №1 getMaxDigit(865513): ${getMaxDigit(865513)}`);
+console.log(`
+function №1 getMaxDigit(865513): ${getMaxDigit(865513)};
+function №2 calculatePower(8,3): ${calculatePower(8,3)}`);
