b7b3c8f8ec97e82aea4ea2c804521c6575476333
unknown <romanna.d@gmail.com>
2020-11-23 17:03:13 +0200

    ready to verifying

--- HW_04/script_04.js
+++ HW_04/script_04.js
@@ -1,6 +1,11 @@
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"]; 
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
 const marks = [4, 5, 5, 3, 4, 5];
+
+
+const girlsNames = ["Олена", "Іра", "Світлана"];
+const boysNames = ["Олександр", "Ігор", "Олексій"]
+const minMark = 1, maxMark = 5;
 
 const combineNewArray = (arr1, arr2) => {
 	let resultArr = [];
@@ -10,31 +15,37 @@ const combineNewArray = (arr1, arr2) => {
 	return resultArr;
 };
 
-const getPairs = (arr) => {
+const joinElements = (arr) =>{
+	let newArr = [];
+	for (let item of arr){
+		newArr.push(`${item[0]} і ${item[1]}`)
+	}
+	return newArr;
+}
+
+const getPairs = (arr, namesM, namesW) => {
 	let gerlsArr = [];
 	let boysArr = [];
-	for(let i = 0; i < arr.length; i++){
-		if (arr[i] === "Олена" || arr[i] === "Іра" || arr[i] === "Світлана"){
-			gerlsArr.push(arr[i]);
-		} else {
-			boysArr.push(arr[i])
-		};
+	for(let i = 0; i < arr.length; i++){		
+		if (namesM.includes(arr[i])) boysArr.push(arr[i]);
+		if (namesW.includes(arr[i])) gerlsArr.push(arr[i]);		
 	};
 	return combineNewArray(boysArr, gerlsArr)
-};
+}; 
 
 const getMarksForPears = (pearsArr) => {
 	let marksArr = [];
 	for(let i = 0; i < pearsArr.length; i++){
-		marksArr.push(Math.floor(Math.random() * 4) + 1);
+		marksArr.push(Math.floor(Math.random() * (maxMark- minMark) + minMark));
 	}
 	return combineNewArray(pearsArr, marksArr);
 };
 
-const pairs = getPairs(students);
-const themesForPairs = combineNewArray(pairs, themes);
+const pairs = getPairs(students, boysNames, girlsNames);
+const themesForPairs = combineNewArray(joinElements(pairs), themes);
 const MarksForStudents = combineNewArray(students, marks);
-const marksForPears = getMarksForPears(pairs);
+const marksForPears = getMarksForPears(joinElements(pairs));
+
 console.log(pairs);
 console.log(themesForPairs);
 console.log(MarksForStudents);
