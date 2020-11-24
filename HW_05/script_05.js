316f98bec4defea26c418f2ce1374c51621acaf2
unknown <romanna.d@gmail.com>
2020-11-24 23:14:33 +0200

    functions #1,#3,#5,#6,#7 are done

--- HW_05/script_05.js
+++ HW_05/script_05.js
@@ -7,12 +7,31 @@ const getRandomArray = (length, min, max) =>{
 	return randomArr;
 };
 
-//console.log(getRandomArray(10, 0, 40));
+//3
+const getAverage = (...numbers) => {
+	let integerNumbers = numbers.filter(number =>{return Math.trunc(number) === number});	
+	return (integerNumbers.reduce((sum, number) => sum + number, 0))/integerNumbers.length;
+};
 
 //5
-
-const  filterEvenNumbers = function(...numbers){
+const  filterEvenNumbers = (...numbers) => {
 	return numbers.filter(number =>{return number%2 === 1})
 };
-console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6));
 
+//6 
+const countPositiveNumbers = (...numbers) =>{
+	let positiveNumbers = numbers.filter(number => {return number>0});
+	return positiveNumbers.length;
+};
+
+//7
+const getDividedByFive = (...numbers) => {
+	return numbers.filter(number =>{return number%5 === 0})
+};
+
+console.log(`
+function #1 getRandomArray(10, 0, 40):   ${getRandomArray(10, 0, 40)};
+function #3 getAverage(6, 2, 55, 11, 78, 22.14, 2, 55, 77, 57, 56.12, 87, 23, 2, 56, 3, 2):   ${getAverage(6, 2, 55, 11, 78, 22.14, 2, 55, 77, 57, 56.12, 87, 23, 2, 56, 3, 2)}
+function #5 filterEvenNumbers(1, 2, 3, 4, 5, 6):    ${filterEvenNumbers(1, 2, 3, 4, 5, 6)}
+function #6 countPositiveNumbers(1, -2, 3, -4, -5, 6):    ${countPositiveNumbers(1, -2, 3, -4, -5, 6)}
+function #7 getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2):    ${getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)} `);
