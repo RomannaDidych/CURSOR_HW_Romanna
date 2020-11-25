6e804a99346feb8ac9acc0f21b75ccc3516c4959
unknown <romanna.d@gmail.com>
2020-11-25 21:36:50 +0200

    added function #9 divideByThree

--- HW_05/script_05.js
+++ HW_05/script_05.js
@@ -9,8 +9,7 @@ const getRandomArray = (length, min, max) =>{
 
 //2 getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)
 /*const getModa = (...numbers) => {
-	let integerNumbers = numbers.filter(number =>{return Math.trunc(number) === number});
-	const numberAndCount = integerNumbers
+	const integerNumbers = numbers.filter(number =>{return Math.trunc(number) === number});	
 }*/
 
 //3
@@ -55,10 +54,54 @@ const getDividedByFive = (...numbers) => {
 	return numbers.filter(number =>{return number%5 === 0})
 };
 
+//8
+const badWords = ['shit', 'fuck'];
+const replaceBadWords = (string) =>{
+	const changeBadWord = (word, startInd, endInd) => {		
+		return  word.split('').
+				map((char,index) => {
+					if((index >= startInd) && (index <=endInd)) char = '*';
+					return char;
+				}).
+				join('');
+	};
+	let arrWithBadWords = string.split(' ');
+	let arrNiceWordsOnly = [];
+	for (let i=0;i<badWords.length; i++){
+		arrNiceWordsOnly = arrWithBadWords.map(word =>{			
+			const bedInd = word.indexOf(badWords[i]); 
+			if(bedInd !== -1) word = changeBadWord(word, bedInd, bedInd + badWords[i].length -1);			 
+			return word;
+		});
+		arrWithBadWords = arrNiceWordsOnly;
+	};
+	return arrNiceWordsOnly.join(' ');
+};
+
+//9 divideByThree("Commander") -> ["com", "man", "der"]
+const divideByThree = (string) =>{
+	
+	stringArr = string.toLowerCase().split('').filter(char => char !== ' ');	
+	const divider = 3;
+	let dividedArr = [];
+	while(stringArr.length >= divider){
+		let shortArr = [];
+		for(let i=0; i< divider; i++ ){
+			shortArr.push(stringArr.shift())
+		}
+		dividedArr.push(shortArr.join(''));		
+	};
+	if(stringArr.length > 0) dividedArr.push(stringArr.join(''));
+	return dividedArr;
+};
+
 console.log(`
 function #1 getRandomArray(10, 0, 40):   ${getRandomArray(10, 0, 40)};
 function #3 getAverage(6, 2, 55, 11, 78, 22.14, 2, 55, 77, 57, 56.12, 87, 23, 2, 56, 3, 2):   ${getAverage(6, 2, 55, 11, 78, 22.14, 2, 55, 77, 57, 56.12, 87, 23, 2, 56, 3, 2)};
 function #4 getMedian(6, 2, 55, 11, 78, 2, 55, 77, 77.5, 14.22, 57, 87, 23, 2, 56, 3, 2):    ${getMedian(6, 2, 55, 11, 78, 2, 55, 77, 77.5, 14.22, 57, 87, 23, 2, 56, 3, 2)};
 function #5 filterEvenNumbers(1, 2, 3, 4, 5, 6):    ${filterEvenNumbers(1, 2, 3, 4, 5, 6)}
 function #6 countPositiveNumbers(1, -2, 3, -4, -5, 6):    ${countPositiveNumbers(1, -2, 3, -4, -5, 6)}
-function #7 getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2):    ${getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)} `);
+function #7 getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2):    ${getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)};
+function #8 replaceBadWords("Are you bigshit fuck fucking kidding?"):    ${replaceBadWords("Are you bigshit fuck fucking kidding?")};
+function #9 divideByThree(" Com  mand er er"):   ${divideByThree(" Com  mand er er")} `)
+
