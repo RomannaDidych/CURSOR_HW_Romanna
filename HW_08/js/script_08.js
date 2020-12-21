95d3fe692716e4faf7b5fbd4f370dfa972ccc3d3
unknown <romanna.d@gmail.com>
2020-12-22 00:05:42 +0200

    fixed getMarks

--- HW_08/js/script_08.js
+++ HW_08/js/script_08.js
@@ -1,55 +1,57 @@
 class Students {
-	constructor (university, course, fullName){
-		this.university = university;
-		this.course = course;
-		this.fullName = fullName;
-		this.marks = [];
-		this.isStudent = true;		 
-	}
+  constructor(university, course, fullName) {
+    this.university = university;
+    this.course = course;
+    this.fullName = fullName;
+    this.marks = [];
+    this.isStudent = true;
+  }
 
-	get getStudentInfo(){return `Студент ${this.course}-го курсу ${this.university} ${this.fullName}`};
+  get getStudentInfo() {
+    return `Студент ${this.course}-го курсу ${this.university} ${this.fullName}`;
+  }
 
-	get getAverage(){
-		if(this.marks.length){
-			return (this.marks.reduce((sum, number) => sum + number, 0))/this.marks.length;
-		} else{			
-			return null
-		}
-	}
+  get getAverage() {
+    if (this.marks.length) {
+      return (
+        this.marks.reduce((sum, number) => sum + number, 0) / this.marks.length
+      );
+    } else {
+      return null;
+    }
+  }
 
-	get getMarks(){		
-		if(this.isStudent){
-			return this.marks
-		} else {
-			console.log(`Sorry! ${this.fullName} isn't a student now`)
-			return null
-		}
-	}
+  get getMarks() {
+    return this.isStudent ? this.marks : null;
+  }
 
-	set setMark(mark){		
-		if(this.isStudent) {
-			this.marks.push(mark);
-			this.getMarks;
-		} else {
-			console.log(`Вибачте! ${this.fullName} не є нашим студентом`)
-		}
-	}
+  set setMark(mark) {
+    if (this.isStudent) {
+      this.marks.push(mark);
+      this.getMarks;
+    } else {
+      console.log(`Вибачте! ${this.fullName} не є нашим студентом`);
+    }
+  }
 
-	dismiss(){
-		this.isStudent = false;
-		this.marks = [];
-	}
+  dismiss() {
+    this.isStudent = false;
+    this.marks = [];
+  }
 
-	recover(){
-		this.isStudent = true;
-		console.log(`Вітаємо! ${this.fullName} знову став студентом`)
-	}
-	 
+  recover() {
+    this.isStudent = true;
+    console.log(`Вітаємо! ${this.fullName} знову став студентом`);
+  }
 }
 
-const ostap = new Students('Вища Школа Психотерапії м.Одеса', '1', 'Остап Бендер');
-console.log(ostap)
-console.log(ostap.getStudentInfo)
+const ostap = new Students(
+  "Вища Школа Психотерапії м.Одеса",
+  "1",
+  "Остап Бендер"
+);
+console.log(ostap);
+console.log(ostap.getStudentInfo);
 console.log(ostap.getAverage);
 console.log(ostap.getMarks);
 ostap.setMark = 5;
@@ -60,30 +62,36 @@ ostap.dismiss();
 console.log(ostap);
 console.log(ostap.getMarks);
 ostap.recover();
-console.log(ostap)
-
+console.log(ostap);
 
 const delay = 30000;
 const scholarshipValue = 1400;
 const minAverage = 4.0;
 
 class BudgetStudent extends Students {
-	constructor(university, course, fullName, scholarship){
-		super(university, course, fullName);
-		this.scholarship = 0;
+  constructor(university, course, fullName, scholarship) {
+    super(university, course, fullName);
+    this.scholarship = 0;
 
-		let intID = setInterval(() => {if((this.getAverage >= minAverage) && this.isStudent){
-										this.getScholarship;
-										console.log(`Ви отримали ${this.scholarship} грн. стипендії`)
-										} else {this.scholarship = 0}
-									} , delay);
-	}
-	get getScholarship(){
-			return this.scholarship = scholarshipValue;
-		}
+    let intID = setInterval(() => {
+      if (this.getAverage >= minAverage && this.isStudent) {
+        this.getScholarship;
+        console.log(`Ви отримали ${this.scholarship} грн. стипендії`);
+      } else {
+        this.scholarship = 0;
+      }
+    }, delay);
+  }
+  get getScholarship() {
+    return (this.scholarship = scholarshipValue);
+  }
 }
 
-const ira = new BudgetStudent('Національний Університет м.Львів', 4, 'Ірина Решетняк');
+const ira = new BudgetStudent(
+  "Національний Університет м.Львів",
+  4,
+  "Ірина Решетняк"
+);
 console.log(ira);
 console.log(ira.getStudentInfo);
 console.log(ira.getMarks);
