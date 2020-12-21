401d634652b13ed9928d54000ee90492cac988f3
unknown <romanna.d@gmail.com>
2020-12-21 23:29:37 +0200

    fixed startAudio and added stopAudio

--- HW_10/js/script_10.js
+++ HW_10/js/script_10.js
@@ -1,28 +1,40 @@
-const buttonArr =  Array.from(document.querySelectorAll('.button')); 
-const melodyArr = Array.from(document.querySelectorAll('.melody'));
+const buttonArr = Array.from(document.querySelectorAll(".button"));
+const melodyArr = Array.from(document.querySelectorAll(".melody"));
 
-const removeActive = () => buttonArr.forEach(btn => btn.classList.remove('active'));
-const selectAudio = name => melodyArr.filter((sound) => sound.classList.contains(name))[0];		
+const removeActive = () =>
+  buttonArr.forEach((btn) => btn.classList.remove("active"));
+const selectAudio = (name) =>
+  melodyArr.filter((sound) => sound.classList.contains(name))[0];
+const stopAudio = () => melodyArr.forEach((sound) => sound.pause());
+const startAudio = (audio) => {
+  audio.currentTime = 0;
+  audio.play();
+};
 
+buttonArr.forEach((btn) => btn.addEventListener("click", connectBtnWithAudio));
+function connectBtnWithAudio() {
+  stopAudio();
+  removeActive();
+  this.classList.add("active");
+  const btnName = this.getAttribute("data-btnName");
+  startAudio(selectAudio(btnName));
+}
 
-buttonArr.forEach((btn) => btn.addEventListener('click', connectBtnWithAudio));	
-	function connectBtnWithAudio(){		
-		removeActive();
-		this.classList.add('active');
-		const btnName = this.getAttribute('data-btnName');		
-		selectAudio(btnName).play();
-	};
-
-document.addEventListener('keydown', connectPressedBtnWithAudio);
-	function connectPressedBtnWithAudio(e){		
-		const btnCode = e.code;				
-		const condition = buttonArr.map(li => li.getAttribute('data-btnName')).includes(btnCode);		
-		if (!condition) return;
-		removeActive();		
-		const pressedBtn = buttonArr.filter(btn => btn.getAttribute('data-btnName') === btnCode)[0];
-		pressedBtn.classList.add('active');		
-		selectAudio(btnCode).play();		
-	};
+document.addEventListener("keydown", connectPressedBtnWithAudio);
+function connectPressedBtnWithAudio(e) {
+  const btnCode = e.code;
+  const condition = buttonArr
+    .map((li) => li.getAttribute("data-btnName"))
+    .includes(btnCode);
+  if (!condition) return;
+  stopAudio();
+  removeActive();
+  const pressedBtn = buttonArr.filter(
+    (btn) => btn.getAttribute("data-btnName") === btnCode
+  )[0];
+  pressedBtn.classList.add("active");
+  startAudio(selectAudio(btnCode));
+}
 
 
 
