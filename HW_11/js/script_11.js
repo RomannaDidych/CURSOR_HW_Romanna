d997a1717673ca61434dfe64d74f0667bd8870cf
unknown <romanna.d@gmail.com>
2020-12-22 18:41:41 +0200

    added link for favicon.ico

--- HW_11/js/script_11.js
+++ HW_11/js/script_11.js
@@ -1,25 +1,25 @@
-async function getRandomChinese(length){
-	const delay = 50;	
-	let chineseStr = '';
+async function getRandomChinese(length) {
+  const delay = 50;
+  let chineseStr = "";
 
-	const makeChinesChar = () =>{		
-		const sign = +(String(Date.now()).slice(-5));		
-		return String.fromCharCode(sign);
-	}
+  const makeChinesChar = () => {
+    const sign = +String(Date.now()).slice(-5);
+    return String.fromCharCode(sign);
+  };
 
-	let getRandomChar = () => new Promise(
-		(resolve) => setTimeout(() => {			
-			return resolve(makeChinesChar())
-		},delay)
-	);
+  let getRandomChar = () =>
+    new Promise((resolve) =>
+      setTimeout(() => {
+        return resolve(makeChinesChar());
+      }, delay)
+    );
 
-	while(length>0){
-		chineseStr += await getRandomChar();		
-		length--;
-	};
-	return chineseStr;	
-};
+  while (length > 0) {
+    chineseStr += await getRandomChar();
+    length--;
+  }
+  return chineseStr;
+}
 getRandomChinese(4).then((result) => console.log(result));
-
 
 
